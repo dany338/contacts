1bff3f21447602787d8ce7d1406a1e8791bf57b1
Daniel Andres Gallo Garcia <dany338@gmail.com>
2022-05-29 23:30:30 -0500

    new changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "624aa166d04f24d5"; }
+/******/ 		__webpack_require__.h = function() { return "4b207fba4cf264b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
