668078530e5b3cb02f82d84ca1ca4feafb4bb867
Daniel Andres Gallo Garcia <dany338@gmail.com>
2022-05-29 23:40:51 -0500

    add new changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b207fba4cf264b1"; }
+/******/ 		__webpack_require__.h = function() { return "19fd5e16f2c2a4a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- firebase/index.ts
+++ firebase/index.ts
@@ -1,5 +1,3 @@
 import firebase from "./firebase";
-import FirebaseContext from "../firebase/context";
 
-export { FirebaseContext };
 export default firebase;
